6eb1b21d789d3c666abba4cf2cdd9f847b52fa54
Cesar <cesarreis521@gmail.com>
2022-02-04 15:17:44 -0300

    Paginas de teste em Angular

---
+++ src/app/adicionar-vagas/adicionar-vagas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-vagas',
+  templateUrl: './adicionar-vagas.component.html',
+  styleUrls: ['./adicionar-vagas.component.css']
+})
+export class AdicionarVagasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/excluir-vagas/excluir-vagas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-vagas',
+  templateUrl: './excluir-vagas.component.html',
+  styleUrls: ['./excluir-vagas.component.css']
+})
+export class ExcluirVagasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
